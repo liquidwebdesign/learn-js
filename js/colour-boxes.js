f32f263091810e2d705c21d34803174fbb78f767
Carl Osterly <carl@liquidweb.com.au>
2014-05-30 21:13:51 +1000

    First implementation of colour-boxes.js

--- js/colour-boxes.js
+++ js/colour-boxes.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
 
+	for (var i=0; i<=62; i++) {
+		$('#configure-grid').append('<div class="cell"> </div>');
+	}
+
+	$('.cell').mouseover(function (event) {
+		var randomColour = '#' + Math.random().toString(16).slice(2,8);
+		$(this).css('background-color', randomColour);
+	});
+
 });
