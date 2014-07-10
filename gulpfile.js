f7ea555913df53aa49472b8ac27a1b9e487aa0ba
Carl Osterly <carl@liquidweb.com.au>
2014-07-10 21:25:56 +1000

    Installed local copy of gulp-autoprefixer and updated gulp.js config file

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 // Include Our Plugins
 
 var compass = require('gulp-compass');
-
+var autoprefixer = require('gulp-autoprefixer');
 var livereload = require('gulp-livereload');
 
 gulp.task('compass', function() {
@@ -14,6 +14,7 @@ gulp.task('compass', function() {
 		css: 'css',
 		sass: 'scss'
 	}))
+	.pipe(autoprefixer('last 1 version', 'android 4'))
 	.pipe(gulp.dest('css'));
 });
 
