c1e2f71f48ac2a42790ecf04f39d1f69e05dee98
Carl Osterly <carl@liquidweb.com.au>
2014-08-18 19:18:39 +1000

    Click counts up and refreshes each second
    Click counts up and refreshes each second

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-	var today = new Date();
-	var hour = today.getHours();
-	var minutes = today.getMinutes();
-	var seconds = today.getSeconds();
-	$('h2#currentTime').text(hour + ':' + minutes + ':' + seconds);
+
+	setInterval( function () {
+		var today = new Date();
+		var hour = today.getHours();
+		var minutes = today.getMinutes();
+		var seconds = today.getSeconds();
+		$('h2#currentTime').text(hour + ':' + minutes + ':' + seconds);
+	},1000);
+
 });
