32e1151724b361a090112f481189799e846aad77
Carl Osterly <carl@liquidweb.com.au>
2016-02-24 07:21:35 +1100

    Add missing bracket to conditional statement.
    Add assignment example under Operators and Expressions section.

--- js/training/conditional.js
+++ js/training/conditional.js
@@ -1,6 +1,17 @@
+// Conditional statement
+
 a = 49;
 
 if (a < 50) {
 	console.log("This number is less than 50.");
 } else {
 	console.log("This number is greater than 50.");
+}
+
+// Operators and Expressions
+
+var1 = 50;
+var2 = 50;
+result = var1 + var2;
+console.log(result);
+
