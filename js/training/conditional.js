7c83195fad6650e812b99adb7a7f8a8819ff0d35
Carl Osterly <carl@liquidweb.com.au>
2016-02-29 06:42:11 +1100

    Added equality statements to conditional examples.

--- js/training/conditional.js
+++ js/training/conditional.js
@@ -15,3 +15,28 @@ var2 = 50;
 result = var1 + var2;
 console.log(result);
 
+// Arithmetic Operators
+
+result += 100;
+console.log(result);
+
+// Operator Precedence
+
+operatorResult = 5 + 5 * 10;
+console.log(operatorResult);
+
+// Change the order of precedence
+
+operatorResult = (5 + 5) * 10;
+console.log(operatorResult);
+
+// Equality
+
+var1 = 50;
+var2 = 50;
+if (var1 === var2) {
+	console.log("They are equal!");
+} else {
+	console.log("They are not equal!");
+}
+
